d2b3a1e7255f8330c646f12df669f9de541f5065
MIZiper <miziper@163.com>
2023-04-27 22:39:33 +0200

    init structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tpl",
+    description="Test planner and logger",
+    author="MIZiper",
+    author_email="miziper@163.com",
+    url="http://mizip.net/",
+    download_url="https://github.com/MIZiper/tpl.git",
+    license="Apache-2.0",
+    packages=find_packages(),
+)
