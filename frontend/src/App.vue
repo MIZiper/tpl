8c337a7eaac8a50ac92ec6fac010ade13b0be9ef
MIZiper <miziper@163.com>
2024-09-24 14:05:20 +0200

    extract mainpage out of app
    to debug component individually

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,38 +1,12 @@
 <template>
   <v-app>
-    <v-app-bar>
-      <v-app-bar-title>
-        <v-icon>mdi-clipboard-text</v-icon>
-        Test Planner & Logger
-      </v-app-bar-title>
-    </v-app-bar>
-
-    <v-navigation-drawer expand-on-hover rail>
-      <v-list>
-        <v-list-item title="Home" prependIcon="mdi-home" />
-        <v-list-item title="Bricks" prependIcon="mdi-wall" />
-        <v-list-item title="Planner" prependIcon="mdi-calendar" />
-        <v-list-item title="Logger" prependIcon="mdi-clipboard-clock" />
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-main>
-      <TestPlanner />
-    </v-main>
-
-    <v-navigation-drawer location="right">
-      <v-list>
-        <v-list-subheader>Tools</v-list-subheader>
-        <v-divider></v-divider>
-        <v-list-item>Tool A</v-list-item>
-        <v-list-item>Tool B</v-list-item>
-        <v-list-item>Tool C</v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
+    <TestPlanner />
   </v-app>
 </template>
 
 <script setup lang="ts">
+
+import MainPage from './MainPage.vue';
+
 //
 </script>
